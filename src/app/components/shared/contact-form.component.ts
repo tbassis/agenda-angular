84dff5fa12a14fa325d9cb069012fc5d719d9f7d
tbassis <tulioassis.18@gmail.com>
2025-03-30 23:29:01 -0300

    Implementação inicial da tela de editar usuários

---
+++ src/app/components/shared/contact-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Contact } from '../../models/contact';
+
+@Component({
+  standalone: true,
+  selector: 'contact-form-component',
+  templateUrl: './contact-form.component.html',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+})
+export class ContactFormComponent {
+  @Input() initialData: Contact | null = null;
+  @Output() formSubmitted = new EventEmitter<Contact>();
+
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      celular: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      telefone: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      favorito: ['N', Validators.required]
+    });
+  }
+
+  // Getter/Setter para o checkbox
+  get favoriteCheckbox(): boolean {
+    return this.contactForm.get('favorito')?.value === 'S';
+  }
+
+  set favoriteCheckbox(value: boolean) {
+    this.contactForm.get('favorito')?.setValue(value ? 'S' : 'N');
+  }
+
+  onFavoriteCheckboxChange(event: Event) {
+    this.favoriteCheckbox = (event.target as HTMLInputElement).checked;
+  }
+
+  ngOnChanges() {
+    if (this.initialData) {
+      this.contactForm.patchValue({
+        ...this.initialData,
+        favorito: this.initialData.favorito || 'N' // Default para 'N'
+      });
+    }
+  }
+
+  onSubmit() {
+    
+    if (this.contactForm.valid) {
+      const formData = {
+        ...this.contactForm.value,
+        favorito: this.contactForm.value.favorito
+      };
+      this.formSubmitted.emit(formData);
+    }
+  }
+}
